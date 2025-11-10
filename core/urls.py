19bfe34101195539fdb094a212f96159f0d4d54a
Kwerq <denisushinskii.stim.gg.yt@gmail.com>
2025-11-10 11:19:36 +0300

    add urlpatterns normally

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,18 @@
-from django.urls import path, re_path
-from django.views.generic.detail import DetailView
-
+from django.urls import path
+from django.views.generic import DetailView
 from core.views import IndexView
+from core.models import Question
 
 urlpatterns = [
-    path('', IndexView.as_view()),
-    path('/questions/<int:id>', DetailView.as_view()),
+    path('', IndexView.as_view(), name='index'),
+    path(
+        'questions/<int:id>/',
+        DetailView.as_view(
+            model=Question,
+            template_name='core/index.html',
+            context_object_name='question',
+            pk_url_kwarg='id'
+        ),
+        name='question_detail'
+    ),
 ]
